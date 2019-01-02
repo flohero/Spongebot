299addb2cf9400be312d6f4914935c826e4d176f
flohero <flohero1@gmail.com>
2019-01-02 21:23:39 +0100

    Removed unused function

--- api/api.go
+++ api/api.go
@@ -15,41 +15,23 @@ type Controller struct {
 func Serve(persistence *database.Persistence) {
 	c := &Controller{persistence: persistence}
 	r := mux.NewRouter()
+
 	// API
 	r.Use(corsAndContentTypeHeader)
 	r.Use(c.JwtAuthentication)
 
-	r.HandleFunc("/api/commands", c.GetAllCommands).Methods("GET")
-	r.HandleFunc("/api/commands", c.CreateCommand).Methods("POST")
-	r.HandleFunc("/api/commands/{id}", c.GetCommandById).Methods("GET")
+	r.HandleFunc("/api/commands", c.GetAllCommands).Methods("GET", "OPTIONS")
+	r.HandleFunc("/api/commands", c.CreateCommand).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/commands/{id}", c.GetCommandById).Methods("GET", "OPTIONS")
 
-	r.HandleFunc("/api/configs", c.GetAllConfigs).Methods("GET")
-	r.HandleFunc("/api/configs", c.CreateConfig).Methods("POST")
-	r.HandleFunc("/api/configs/{id}", c.GetConfigById).Methods("GET")
+	r.HandleFunc("/api/configs", c.GetAllConfigs).Methods("GET", "OPTIONS")
+	r.HandleFunc("/api/configs", c.CreateConfig).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/configs/{id}", c.GetConfigById).Methods("GET", "OPTIONS")
 
-	r.HandleFunc("/api/user/new", c.CreateAccount).Methods("POST")
-	r.HandleFunc("/api/user/login", c.Authenticate).Methods("POST")
-
-	// CORS
-
-	r.HandleFunc("/api/commands", c.options).Methods("OPTIONS")
-	r.HandleFunc("/api/commands", c.options).Methods("OPTIONS")
-	r.HandleFunc("/api/commands/{id}", c.options).Methods("OPTIONS")
-
-	r.HandleFunc("/api/configs", c.options).Methods("OPTIONS")
-	r.HandleFunc("/api/configs", c.options).Methods("OPTIONS")
-	r.HandleFunc("/api/configs/{id}", c.options).Methods("OPTIONS")
-
-	r.HandleFunc("/api/user/new", c.options).Methods("OPTIONS")
-	r.HandleFunc("/api/user/login", c.options).Methods("OPTIONS")
+	r.HandleFunc("/api/user/new", c.CreateAccount).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/user/login", c.Authenticate).Methods("POST", "OPTIONS")
 
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	panic(http.ListenAndServe(":8080", loggedRouter))
 
-}
-
-func (c *Controller) options(w http.ResponseWriter, r *http.Request) {
-	w.Header().Del("Content-Type")
-	println(w.Header().Get("Content-Type"))
-	w.WriteHeader(200)
 }
